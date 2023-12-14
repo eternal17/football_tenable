b3494fd0813616431713909e18e91cc95361e7f9
eternal17 <sarmadkhatri15@gmail.com>
2023-12-14 16:52:33 +0000

    fronend for triangle and answer divs

--- src/Quizpage.tsx
+++ src/Quizpage.tsx
@@ -4,7 +4,25 @@ interface QuizProps {
 }
 
 const QuizPage: React.FC<QuizProps> = ({ quizArr }) => {
-  return <div className="quiz-container"></div>;
+  return (
+    <div className="quiz-container">
+      <div className="triangle-answers">
+        <div className="triangle"></div>
+        <div className="answers">
+          <div className="answer-row">hidden answer</div>
+          <div className="answer-row">2</div>
+          <div className="answer-row">3</div>
+          <div className="answer-row">4</div>
+          <div className="answer-row">5</div>
+          <div className="answer-row">6</div>
+          <div className="answer-row">7</div>
+          <div className="answer-row">8</div>
+          <div className="answer-row">9</div>
+          <div className="answer-row correct-answer">correct answer</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default QuizPage;
